e670974a1622802b002aa9f0920ece50e2a08801
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-09 19:31:03 -0400

    added nodemon for start and code in index.js for port

--- lab-3/index.js
+++ lab-3/index.js
@@ -1,2 +1,14 @@
 //importing express
 const express = require('express');
+
+const app = express();
+
+//initialize port
+const port = 3000;
+
+//starting the serer code
+app.listen(port, ()=>{
+console.log(`server is running on http://localhost:${port}`);
+
+})
+
