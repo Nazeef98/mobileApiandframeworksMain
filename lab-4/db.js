a35978fda1a617bff8f3910337f74d5fc7ee771d
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-11 11:48:13 -0400

    Db connection

---
+++ lab-4/db.js
@@ -0,0 +1,18 @@
+//this code will run to check that DB is connected of not
+const mongoose = require("mongoose");
+const MONGOURI =process.env.MONGO_URI;
+
+const InitiateMongoServer = async()=>{
+    try{
+        await mongoose.connect(MONGOURI);
+    
+    console.log("connected to DB");
+    
+    }catch(e){
+        console.log(e);
+        throw(e);
+        }
+    };
+
+    module.exports= InitiateMongoServer;
+
