e594e87ede6e4f639a8b7b8927b1c5caba14e78a
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-13 01:15:49 -0400

    create recipe routes defined

--- assignment-2/routes/recipeRoute.js
+++ assignment-2/routes/recipeRoute.js
@@ -16,7 +16,7 @@ const initializeRecipeRoute = () => {
     router.get('/top20', getTopRecipe)
     router.get('/list', getList)
     router.get('/find/:id',recipeById)
-
+    router.post('/create',createNewRecipe)
     return router
 }
 // Export the initializeRecipeRoute
