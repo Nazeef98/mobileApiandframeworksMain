d42006363fa26417e061262375daca5ccd6ae238
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-18 15:53:44 -0400

    defined the path and added the index.html file that is displaying the basic UI already avilable for showing the records

--- midTerm-MDEV1004/server.js
+++ midTerm-MDEV1004/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 
 //to read the usedData from the JSON file and show it on the local host
 const userData = JSON.parse(fs.readFileSync('./users.json','utf-8'));
@@ -9,7 +10,7 @@ app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 })
 
-//initialised port 
+//initialised port to get from the environment or use the 3000 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log('server is running on '+ port)
