66fec5d82d994cfead75b9265678292a1ddd8bad
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-18 15:52:10 -0400

    getting issue in the code after adding the index.html file

--- midTerm-MDEV1004/server.js
+++ midTerm-MDEV1004/server.js
@@ -1,8 +1,19 @@
 //import express
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
+//to read the usedData from the JSON file and show it on the local host
+const userData = JSON.parse(fs.readFileSync('./users.json','utf-8'));
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+
+//initialised port 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log('server is running on '+ port)
 })
+
+
+
