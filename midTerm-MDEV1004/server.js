d7afde1b5aec801a9eb8d020bb3fa879662aab42
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-18 15:38:59 -0400

    imported express and initialised port

---
+++ midTerm-MDEV1004/server.js
@@ -0,0 +1,8 @@
+//import express
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log('server is running on '+ port)
+})
