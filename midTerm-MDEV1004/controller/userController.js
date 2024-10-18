4162fbab92cc8381d74c6cc9bc63ffecd16a9973
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-10-18 16:49:01 -0400

    getAllUsers and getUserById with proper routing defined and the HTMK button working to call these apis

--- midTerm-MDEV1004/controller/userController.js
+++ midTerm-MDEV1004/controller/userController.js
@@ -1,19 +1,51 @@
 const User = require('../models/User');
 const fs = require('fs')
 
+
+//importing the users method 
 exports.importUsers = async (req, res) => {
     try {
-        const data = JSON.parse(fs.readFileSync('./users.json', 'utf-8')); // Read data from movies.json
+        const data = JSON.parse(fs.readFileSync('./users.json', 'utf-8')); // Reading the data from the user.JSON
         const count = await User.countDocuments();
         if (count === 0) {
-            await User.create(data); // Create User in the database
+            await User.create(data); // Creating the user in the DB 
             console.log('Data successfully imported to MongoDb');
             res.status(200).send('Data successfully added');
         } else {
             res.status(200).send('Data already available');
         }
     } catch (e) {
+        //to highlight error if error in importing th data throught this method 
         console.error('Error importing data', e);
         res.status(500).send('Error importing data');
+    }
+};
+
+//get the users from the list 
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    }
+    catch (e) {
+        console.error(e);
+        res.status(500).send('Error retrieving users');
+    }
+};
+
+
+//the method is used to get the list of the userBy ID 
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findOne({ userId: req.params.id });
+        if (!user) {
+            return res.status(404).send('user is not available');
+        }
+        res.status(201).json(user);
+
+    }
+    catch (e) {
+        console.error(e);
+        res.status(500).send('Error getting the user');
     }
 };
--- midTerm-MDEV1004/route/userRoute.js
+++ midTerm-MDEV1004/route/userRoute.js
@@ -5,5 +5,12 @@ const userController = require('../controller/userController');
 //Route to import users
 userRoute.get('/import',userController.importUsers);
 
+//routes to get the user list 
+userRoute.get('/', userController.getUsers);
+
+//routes to get the user by the ID 
+userRoute.get('/:id',userController.getUserById)
+
+
 
 module.exports = userRoute;
