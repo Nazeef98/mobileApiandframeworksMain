205c0b185c079cd3390f2757977dbaa5dc5047ee
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-11-13 02:39:51 -0500

    assignment-2 code commit to assignment -3 for making further UI for this

---
+++ assignment-3/models/recipe.js
@@ -0,0 +1,21 @@
+// recipe.js
+// Nazeef Ahmad Farooqui
+// 200590966
+// 13/10/24
+
+// models/Recipe.js
+//schema for the API
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    recipeName: { type: String, required: true },
+    ingredients: { type: [String], required: true },
+    cookingTime: { type: Number, required: true },
+    difficulty: { type: String, required: true },
+    cuisine: { type: String, required: true },
+    description: { type: String, required: true },
+    photoLink: { type: String, required: true },
+    averageRating: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
--- assignment-3/server.js
+++ assignment-3/server.js
@@ -1,4 +1,27 @@
 // server.js
 // Nazeef Ahmad Farooqui
 // 200590966
-//13/11/24
+// 13/10/24
+const http = require('http');
+const express = require('express');
+const { log } = require('console');
+const { initializeRecipeRoute } = require('./routes/recipeRoute');
+const connectDB = require('./config/mongodb');
+const { initializeUserRoute } = require('./routes/userRoute');
+const app = express();
+const server = http.createServer(app)
+connectDB()
+app.use(express.json())
+
+//message comes up when server is up and runnign without error
+app.get('/', (req,res) => {
+    res.send('I am learning new things in mobile API and frameworks  ')
+})
+app.use('/recipe',initializeRecipeRoute())
+app.use('/auth', initializeUserRoute())
+
+
+const port = process.env.PORT || 3000;    //getting port from environment variable
+server.listen(port,() => {
+    console.log('Server started and listening to port'+ port);
+})
