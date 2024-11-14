5c1a4c220dbde477ee9510745a089be7405dbfe2
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-11-14 12:57:23 -0500

    added authntication logic to use further in authntication and to return token

--- assignment-3/middleware/authMiddleware.js
+++ assignment-3/middleware/authMiddleware.js
@@ -5,6 +5,18 @@
 
 
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'yourSecretKey'; // Should be in environment variables
+const JWT_SECRET = 'yourSecretKey';
 
 
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(403).json({ message: 'Token is required' });
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
