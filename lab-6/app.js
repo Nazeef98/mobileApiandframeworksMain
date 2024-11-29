0c2755533f7b607c6083080a3b8b0666d9218a39
Nazeef Ahmad <nazeef1998ahmad@gmail.com>
2024-11-29 14:33:45 -0500

    project setup adding files to project

--- lab-6/app.js
+++ lab-6/app.js
@@ -1,2 +1,10 @@
-// hello from app.js
-console.log('hello')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
